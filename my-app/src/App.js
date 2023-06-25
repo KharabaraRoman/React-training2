00a5c62e5b6d4ddc57e6763a7ad457d9bc9980f6
Роман Харабара <117296212+KharabaraRoman@users.noreply.github.com>
2023-06-25 20:34:43 +0200

    Ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,48 +1,93 @@
 import Header from './Header/Header';
 import Counter from './Counter/Counter';
+import React from 'react';
 // import ToDolist from './ToDolist/Todolist'
 import Modal from './Modal/Modal';
-import { Component, useId } from 'react';
+import { useId, useState } from 'react';
 import FormLogin from './FormLogin/FormLogin';
-// import toDo from './ToDolist/todo.json'
 
-class App extends Component {
-  state = {
-    isShowModal: false,
-  };
- 
+import toDo from './ToDolist/todo.json'
 
-  showModal = () => {
-    this.setState({ isShowModal: true });
-  };
+const obj = {
+  name: "Alex",
+  age: 30
+}
 
-  closeModal = () => {
-    this.setState({ isShowModal: false });
-  };
-  createUser = (data) => {
-    const newUser = {
-      ...data,
-      id:useId(),
-    }
-    console.log(newUser)
-  }
-  render() {
-    return (
-      <div className="container">
-        <Header showModal={this.showModal} />
-        <Counter />
-        {/* <ToDolist/> */}
-        {this.state.isShowModal && (
-          <Modal closeModal={this.closeModal}>
-            <FormLogin createUser={this.createUser}
-            closeModal={this.closeModal}/>
-          </Modal>
-        )}
 
-      </div>
-    );
+const showModal = () => {
+  setIsShowModal(true)
+  
+}
+const closeModal = () => {
+  setIsShowModal(false)
+}
+function createUser(data)  {
+  const newUser = {
+    ...data, 
+    id: nanoid(),
   }
 }
+
+const handleSearch = () => {
+  setSearchText(searchText)
+}
+
+
+const App = () => {
+  const [isShowModal, setIsShowModal] = useState(false)
+  const [SearchText, setSearchText] = useState('')
+  const id = nanoid()
+  return (
+    <div className='container'>
+      <Header showModal={showModal} />
+      <Search handleSearch={handleSearch} />
+      <Contentinfo searchText={searchText} />
+      <ToDoList />
+      {this.state.isShowModal && (
+        <Modal closeModal={this.closeModal}>
+          <FormLogin
+            closeModal={this.closeModal}
+            createUser={this.createUser}
+          />
+        </Modal>
+      )}
+    </div>
+  )
+};
+
+
+
+//   showModal = () => {
+//     this.setState({ isShowModal: true });
+//   };
+
+//   closeModal = () => {
+//     this.setState({ isShowModal: false });
+//   };
+//   createUser = (data) => {
+//     const newUser = {
+//       ...data,
+//       id:useId(),
+//     }
+//     console.log(newUser)
+//   }
+//   render() {
+//     return (
+//       <div className="container">
+//         <Header showModal={this.showModal} />
+//         <Counter />
+//         {/* <ToDolist/> */}
+//         {this.state.isShowModal && (
+//           <Modal closeModal={this.closeModal}>
+//             <FormLogin createUser={this.createUser}
+//             closeModal={this.closeModal}/>
+//           </Modal>
+//         )}
+
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
 
